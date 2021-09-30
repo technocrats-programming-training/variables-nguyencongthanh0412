59c552bf46c507829a89ac0703f11412077c0f83
Cong Nguyen <91427158+nguyencongthanh0412@users.noreply.github.com>
2021-09-30 01:03:21 +0000

    Initial work on assignment.

--- Main.java
+++ Main.java
@@ -5,19 +5,20 @@ public class Main {
         String lastName = args[1];
         int age = 0;
         try {
-            int age = Integer.parseInt(args[2]);
+            age = Integer.parseInt(args[2]);
         } catch (NumberFormatException ex) {
             ex.printStackTrace();
         }
         
 
         // Print out the following string: "Hello, I am <firstName> <lastName>, and I am <age> years old"
-
+			System.out.println("Hello, I am "+firstName+" "+lastName+", and I am "+age+" years old");
         // Print out whether age is even
-
+			
         // Print out whether firstName equals lastName
-
+			boolean doesItEqual = "firstName".equals("lastName");
+			System.out.print(doesItEqual);
         // Print out whether age is greater than 0
-
+			
     }
 }
